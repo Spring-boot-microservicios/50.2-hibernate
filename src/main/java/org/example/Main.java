db6105f95c1488c5728810a33b3e796a3e8abb00
Luis Angel <luis.flores@durango.gob.mx>
2024-09-12 12:48:24 -0600

    Generando conexon guardado y muestra de las empleados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try (Session session = HibernateConfig.getSessionFactory().openSession()) {
+
+            final Transaction transaction = session.beginTransaction();
+
+            DepartmentEntity weightDivision = DepartmentEntity.builder()
+                    .name("Lightweight")
+                    .build();
+
+            session.save(weightDivision);
+
+            EmployeeEntity fighter = new EmployeeEntity();
+            fighter.setName("Conor McGregor");
+            fighter.setEmail("conor@ufc.com");
+            fighter.setDepartment(weightDivision);
+
+            session.save(fighter);
+
+            fighter = new EmployeeEntity();
+            fighter.setName("Khabib Nurmagomedov");
+            fighter.setEmail("khabib@ufc.com");
+            fighter.setDepartment(weightDivision);
+
+            session.save(fighter);
+
+            transaction.commit(); // Genera el insert
+
+            // hql => lenguaje de consulta de hibernate
+            // Genera el mapeo
+            Query<EmployeeEntity> query = session.createQuery("FROM EmployeeEntity e JOIN FETCH e.department", EmployeeEntity.class);
+
+            query.list().forEach(System.out::println);
+        }
+
     }
+
 }
