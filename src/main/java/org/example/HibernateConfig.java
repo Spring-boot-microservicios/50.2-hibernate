0f26f8507093116f334a6ce33658b61dcdb23320
Luis Angel <luis.flores@durango.gob.mx>
2024-09-12 12:25:44 -0600

    Configuracion base de hibernate

---
+++ src/main/java/org/example/HibernateConfig.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import lombok.Getter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class HibernateConfig {
+
+    @Getter
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static final String CONFIG_PATH = "src/main/resources/hibernate.properties";
+
+    // Configuracion para agregar las conexiones
+    private static SessionFactory buildSessionFactory() {
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileInputStream(CONFIG_PATH));
+
+            return new Configuration()
+                    .mergeProperties(properties)
+                    .buildSessionFactory();
+        } catch (Throwable throwable) {
+            System.err.println(throwable.getMessage());
+            throw new ExceptionInInitializerError(throwable);
+        }
+
+    }
+
+    // Cerrar la conexion
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+
+}
